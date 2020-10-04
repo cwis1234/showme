458b7f65d0d9c0a71be2c66794286860495e24cb
cwis1234 <cwis1234@gmail.com>
2020-10-04 19:01:39 +0900

    asdf

---
+++ webpj3/src/components/App/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import 'components/App/App.css';
+import {Route,Switch} from "react-router-dom";
+
+
+class App extends Component{
+  constructor() {
+    super();
+  };
+  render(){
+    return(
+      <div>
+        HelloWorld
+      </div>
+    );
+  };
+};
+
+export default App;
